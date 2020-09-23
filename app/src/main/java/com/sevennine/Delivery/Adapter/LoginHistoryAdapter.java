d9124e4efeee387a74440b26f3284497b2a022f3
user <himabindhu.chandhragiri@gmail.com>
2020-09-23 17:03:47 +0530

    keerthiSep23

---
+++ app/src/main/java/com/sevennine/Delivery/Adapter/LoginHistoryAdapter.java
@@ -0,0 +1,117 @@
+package com.sevennine.Delivery.Adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sevennine.Delivery.Bean.Weeklybean3;
+import com.sevennine.Delivery.R;
+import com.sevennine.Delivery.SessionManager;
+
+import java.util.Date;
+import java.util.List;
+
+
+public class LoginHistoryAdapter extends RecyclerView.Adapter<LoginHistoryAdapter.MyViewHolder>  {
+    private List<Weeklybean3> productList;
+    Activity activity;
+    Fragment selectedFragment;
+    SessionManager sessionManager;
+    public LinearLayout linearLayout;
+
+
+    public static CardView cardView;
+    String dtStart;
+    Date date;
+    public LoginHistoryAdapter(Activity activity, List<Weeklybean3> productList) {
+        this.productList = productList;
+        this.activity=activity;
+        sessionManager = new SessionManager(activity);
+
+    }
+
+
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView amount,text;
+        public LinearLayout layout;
+
+
+
+        public MyViewHolder(View view) {
+            super(view);
+            text=view.findViewById(R.id.texttt);
+            amount=view.findViewById(R.id.amountt);
+           // layout=view.findViewById(R.id.linearLayout);
+
+
+        }
+
+    }
+
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.login_adapter, parent, false);
+        return new MyViewHolder(itemView);
+
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, final int position) {
+    final Weeklybean3 products = productList.get(position);
+   holder.text.setText(products.getEarnings());
+   holder.amount.setText(products.getAmount());
+
+
+
+/*
+        holder.layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                products.getId();
+                Bundle bundle = new Bundle();
+                bundle.putString("Ids",products.getId());
+
+                holder.text.setTextColor(Color.parseColor("#fff"));
+                holder.amount.setTextColor(Color.parseColor("#fff"));
+
+                holder.layout.setBackgroundResource(R.drawable.border_empty_curve);
+               // .setBackgroundResource(R.drawable.black_bordr_white_filled1);
+
+            }
+        });*/
+
+
+
+
+
+
+
+    }
+
+
+
+    @Override
+    public int getItemCount() {
+        System.out.println("lengthhhhhhh"+productList.size());
+        return productList.size();
+    }
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+}
---
+++ app/src/main/java/com/sevennine/Delivery/Fragment/RewardsDetails.java
@@ -0,0 +1,138 @@
+package com.sevennine.Delivery.Fragment;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+import android.widget.LinearLayout;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sevennine.Delivery.Adapter.RewardsAdapter;
+import com.sevennine.Delivery.Adapter.WeeklyAdapter;
+import com.sevennine.Delivery.Adapter.WeeklyAdapterRecyc3;
+import com.sevennine.Delivery.Adapter.WeeklyAdapterRecycle2;
+import com.sevennine.Delivery.Bean.Weeklybean;
+import com.sevennine.Delivery.Bean.Weeklybean2;
+import com.sevennine.Delivery.Bean.Weeklybean3;
+import com.sevennine.Delivery.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//Our class extending fragment
+public class RewardsDetails extends Fragment {
+
+    public static RewardsAdapter madapter3;
+
+    public static List<Weeklybean3> newOrderBeansList3= new ArrayList<>();
+    LinearLayout back_feed;
+    Fragment selectedFragment;
+    RecyclerView recyclerView3;
+
+    public static RewardsDetails newInstance() {
+        RewardsDetails itemOnFragment = new RewardsDetails();
+        return itemOnFragment;
+    }
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.rewards, container, false);
+
+
+        recyclerView3=view.findViewById(R.id.reward_recy);
+        back_feed=view.findViewById(R.id.back_feed);
+        // sessionManager = new SessionManager(getActivity());
+        Window window = getActivity().getWindow();
+        window.setStatusBarColor(ContextCompat.getColor(getActivity(), R.color.colorPrimaryDark));
+        view.setFocusableInTouchMode(true);
+        view.requestFocus();
+        view.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                Log.i("ONBACK", "keyCodezzzzzzzzzq  : " + keyCode);
+                if( keyCode == KeyEvent.KEYCODE_BACK && event.getAction() == KeyEvent.ACTION_UP) {
+                    Log.i("ONBACK", "onKey Back listener is working!!!");
+                   /* selectedFragment = AddressDetailFragment.newInstance();
+                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frame_layout1, selectedFragment);
+                    transaction.addToBackStack("dhskswa");
+                    transaction.commit();*/
+                    FragmentManager fm = getFragmentManager();
+                    fm.popBackStack();
+                    return true;
+                }
+                return false;
+            }
+        });
+        back_feed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentManager fm = getFragmentManager();
+                fm.popBackStack();
+            }
+        });
+
+
+
+/////////////////////////////
+
+        newOrderBeansList3.clear();
+        GridLayoutManager mLayoutManager_farm3 = new GridLayoutManager(getActivity(), 1, GridLayoutManager.VERTICAL, false);
+        recyclerView3.setLayoutManager(mLayoutManager_farm3);
+        recyclerView3.setItemAnimator(new DefaultItemAnimator());
+
+        Weeklybean3 weeklybean=new Weeklybean3("₹5","25 July");
+        newOrderBeansList3.add(weeklybean);
+
+        Weeklybean3 weeklybean1=new Weeklybean3("₹13","8 May");
+        newOrderBeansList3.add(weeklybean1);
+
+
+        Weeklybean3 weeklybean2=new Weeklybean3("₹8","9 Jan");
+        newOrderBeansList3.add(weeklybean2);
+
+      Weeklybean3 weeklybean3=new Weeklybean3("₹12","15 May");
+        newOrderBeansList3.add(weeklybean3);
+
+
+        Weeklybean3 weeklybean4=new Weeklybean3("₹14","9 May");
+        newOrderBeansList3.add(weeklybean4);
+        Weeklybean3 weeklybean5=new Weeklybean3("₹17","4 Sep");
+        newOrderBeansList3.add(weeklybean5);
+
+
+
+        madapter3=new RewardsAdapter(getActivity(),newOrderBeansList3);
+        recyclerView3.setAdapter(madapter3);
+
+
+
+
+
+       /* pick_up_arrow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectedFragment = PickupLocationMapFragment.newInstance();
+                FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout_home, selectedFragment);
+                transaction.commit();
+            }
+        });*/
+        return view;
+        
+    }
+
+
+
+}
